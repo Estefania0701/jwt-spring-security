fc5d8f010d96e23c69cb4fa10462c2a44b428fde
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-05-25 10:31:09 -0500

    Implemento JwtIO en AuthService

--- src/main/java/com/eas/tutorial/jwtspringsecurity/service/AuthService.java
+++ src/main/java/com/eas/tutorial/jwtspringsecurity/service/AuthService.java
@@ -1,10 +1,23 @@
 package com.eas.tutorial.jwtspringsecurity.service;
 
 import com.eas.tutorial.jwtspringsecurity.dto.JwtResponse;
+import com.eas.tutorial.jwtspringsecurity.security.JwtIO;
+import com.eas.tutorial.jwtspringsecurity.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    @Autowired
+    private JwtIO jwtIO;
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    @Value("${eas.jwt.token.expired-in}")
+    private int EXPIRED_IN;
 
     public JwtResponse login(String clientId, String clientSecret){
         // autentica el cliente y genera un JWT
@@ -12,10 +25,10 @@ public class AuthService {
         // objeto jwt
         JwtResponse jwt = JwtResponse.builder()
                 .tokenType("bearer")
-                .accessToken("asfdghtsegdfvstrs")
-                .issuedAt(System.currentTimeMillis()+"")
+                .accessToken(jwtIO.generateToken("Hola mundo desde AuthService"))
+                .issuedAt(dateUtil.getDateMillis()+"")
                 .clientId(clientId)
-                .expiresIn(3600) //1 hora
+                .expiresIn(EXPIRED_IN) //1 hora
                 .build();
 
         return jwt;
